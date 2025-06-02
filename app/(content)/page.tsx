38dfdab019351f85b68e0d6bd80f3b76c0f94299
Roman Ernst <roman.ernst@farbenmeer.de>
2025-06-02 17:09:49 +0200

    Optimizes tablet spacings

--- app/(content)/page.tsx
+++ app/(content)/page.tsx
@@ -6,21 +6,21 @@ import Icon from "@/app/icon.svg";
 export default function HomePage() {
   return (
     <main>
-      <section className="bg-green-dark pb-12">
-        <div className="container m-auto markdown flex md:flex-row-reverse flex-col-reverse text-white">
-          <figure className="-ml-24 -mt-6 -mb-20 sm:-my-24 md:mt-0 lg:min-w-lg xl:min-w-2xl relative">
+      <section className="bg-green-dark py-12">
+        <div className="container m-auto flex flex-col-reverse text-white md:flex-row-reverse">
+          <figure className="-mb-20 -mt-6 -ml-12 relative grow md:-my-24 md:mt-0 md:ml-0">
             <Image
               sizes="100vw"
-              className="w-full h-auto"
+              className="h-auto w-full"
               src={HeroImage}
               alt="Das Team von Zukunftsentscheid Hamburg präsentiert den Termin der Volksabstimmung am 12.10.2025 vor der Kulisse des Hamburger Rathauses."
               priority
             />
-            <div className="h3 bg-white text-green-dark transform -rotate-12 absolute top-1/4 -right-2 p-1">
+            <div className="right-10 sm:-right-2 -rotate-12 absolute top-4 sm:top-1/4 bg-white h3 p-1 text-green-dark transform">
               Sag ja
               <Image
                 src={Icon}
-                className="h-7 sm:h-11 w-auto inline"
+                className="h-7 w-auto inline sm:h-11"
                 aria-hidden="true"
                 alt=""
               />
@@ -28,18 +28,15 @@ export default function HomePage() {
               zu Hamburg
             </div>
           </figure>
-          <div className="flex flex-col gap-8 items-start max-w-2xl justify-center relative">
-            <h1 className="h1 sm:hidden">
+          <div className="relative flex flex-col items-start justify-center gap-8">
+            <h1 className="block h1 max-w-[230px] sm:max-w-[420px] lg:hidden">
               Mit dem <mark>Volksentscheid</mark> am 12.10. geben wir Hamburg
-              ein
-              <br />
-              <mark className="short">besseres</mark>{" "}
+              ein <mark className="short">besseres</mark>{" "}
               <mark className="long">Klimaschutzgesetz!</mark>
             </h1>
-            <h1 className="h1 hidden sm:block">
+            <h1 className="hidden h1 max-w-[530px] lg:block">
               Mit dem <mark>Volksentscheid</mark> am 12.10. geben wir Hamburg
-              ein
-              <br />
+              ein <br />
               <mark className="long">besseres Klimaschutzgesetz!</mark>
             </h1>
             <p>Hamburg entscheidet fürs Klima - sei dabei!</p>
@@ -51,7 +48,7 @@ export default function HomePage() {
       </section>
 
       <section className="bg-blue-light">
-        <div className="container py-12 flex flex-col items-center text-center">
+        <div className="container flex flex-col items-center py-12 text-center">
           <h2 className="h2 max-w-xl underline-karl">
             Gemeinsam machen wir Hamburgs Klimapolitik sozial, planbar und
             verantwortungsbewusst.
