ac20ca264078b4b1bc64a466f4b62d1c18663af9
Roman Ernst <roman.ernst@farbenmeer.de>
2025-06-02 09:49:31 +0200

    Introduce lighthouse benchmarks

---
+++ .lighthouserc.js
@@ -0,0 +1,20 @@
+const URL = process.env.LHCI_TARGET_URL || "http://localhost:3000";
+
+module.exports = {
+  ci: {
+    collect: {
+      url: [URL],
+    },
+    assert: {
+      preset: "lighthouse:no-pwa", // this is not an app, it should not use a SW to control the root url.
+      assertions: {
+        "is-crawlable": "off", // Vercel keeps preview URLs non-crawlable and it should stay that way.
+        "unused-css-rules": ["error", { maxLength: 1 }], // can happen with tailwind if some class appears as a string in code that is not actually a css class
+        "categories:performance": ["error", { minScore: 0.5 }],
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
