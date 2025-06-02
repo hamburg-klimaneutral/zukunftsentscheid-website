187758d9de8e9b443b0bcd976c83dc93a9a06fc4
Roman Ernst <roman.ernst@farbenmeer.de>
2025-06-02 09:49:31 +0200

    Introduce lighthouse benchmarks

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
-import createMDX from '@next/mdx'
+import createMDX from "@next/mdx";
 
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ["md", "mdx", "ts", "tsx"],
+  experimental: {
+    inlineCss: true,
+  },
 };
 
 const withMDX = createMDX();
