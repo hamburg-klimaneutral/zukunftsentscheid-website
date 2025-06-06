c2def7adae1063acb75d8a3166735cae9c083d79
Johannes Lamberts <johannes@lamberts.dev>
2025-06-06 11:31:48 +0200

    feat: start implementing donation page

--- app/(content)/spenden/page.tsx
+++ app/(content)/spenden/page.tsx
@@ -3,9 +3,21 @@ import TwingleWidget from "../twingle-widget";
 export default function DonationPage() {
     return (
         <main>
-            <section className="bg-blue-light py-12">
+            <section className="bg-green-dark py-12">
+                <div className="container py-24">
+                    Hello!
+                </div>
+            </section>
+            <section className="py-12">
+                <div className="container py-24">
+                    Wieso spenden?
+                </div>
+            </section>
+            <section>
+                <div className="container py-24">
                 {/* @TODO: use old project name to keep history */}
-                <TwingleWidget projectName="next-test-spenden" projectId="tw683f2bafdc6cb" />
+                    <TwingleWidget projectName="next-test-spenden" projectId="tw683f2bafdc6cb" />
+                </div>
             </section>
         </main>
     )
