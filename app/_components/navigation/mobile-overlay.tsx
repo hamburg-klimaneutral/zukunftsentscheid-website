7f9ea32907766ad3911c5fd1f47861bdb15853eb
Roman Ernst <roman.ernst@farbenmeer.de>
2025-06-08 22:58:10 +0200

    Make mobile navigation scrollable

--- app/_components/navigation/mobile-overlay.tsx
+++ app/_components/navigation/mobile-overlay.tsx
@@ -1,38 +1,42 @@
 import Image from "next/image";
 import * as Dialog from "@radix-ui/react-dialog";
 
-export default function MobileOverlay({ children }: { children: React.ReactNode }) {
+export default function MobileOverlay({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <Dialog.Root>
       <Dialog.Portal>
         <Dialog.Overlay className="lg:hidden fixed inset-0 h-dvh w-screen bg-green-dark flex flex-col justify-between pt-30 z-20">
-          <Dialog.Content className="w-full h-full">
+          <Dialog.Content className="w-full h-full overflow-y-scroll">
             <Dialog.Title className="sr-only">Navigation</Dialog.Title>
             {children}
+            <div className="absolute top-0 py-6 px-5 flex w-full justify-between">
+              <figure>
+                <Image
+                  src="/logo.svg"
+                  alt="Zukunftsentscheid-Hamburg Logo"
+                  width={213}
+                  height={58}
+                />
+              </figure>
+              <Dialog.Close
+                className="select-none"
+                aria-label="Navigation schließen"
+              >
+                <Image
+                  src="/icons/close.svg"
+                  width={48}
+                  height={48}
+                  className="w-12 h-12"
+                  alt=""
+                  aria-hidden
+                />
+              </Dialog.Close>
+            </div>
           </Dialog.Content>
-          <div className="absolute top-0 py-6 px-5 flex w-full justify-between">
-            <figure>
-              <Image
-                src="/logo.svg"
-                alt="Zukunftsentscheid-Hamburg Logo"
-                width={213}
-                height={58}
-              />
-            </figure>
-          <Dialog.Close
-            className="select-none"
-            aria-label="Navigation schließen"
-          >
-            <Image
-              src="/icons/close.svg"
-              width={48}
-              height={48}
-              className="w-12 h-12"
-              alt=""
-              aria-hidden
-            />
-          </Dialog.Close>
-          </div>
         </Dialog.Overlay>
       </Dialog.Portal>
       <Dialog.Trigger
