f292e2b0d2a15fad39f9c9fd91f5fe797640de9f
Johannes Lamberts <johannes@lamberts.dev>
2025-06-13 09:11:16 +0200

    refactor: explicit props for PressReleaseHeroBlock

--- app/(content)/presse/hero-block.tsx
+++ app/(content)/presse/hero-block.tsx
@@ -1,19 +1,20 @@
-import type { PressRelease } from "@/app/(content)/presse/type";
 import Block from "@/app/_components/block";
 import Hero from "@/app/_components/hero";
 
 interface PressReleaseHeroBlockProps {
-  pressRelease: PressRelease;
+  title: string;
+  date: string;
 }
 
-export default function PressReleaseHeroBlock(
-  props: PressReleaseHeroBlockProps
-) {
+export default function PressReleaseHeroBlock({
+  title,
+  date,
+}: PressReleaseHeroBlockProps) {
   return (
     <Block variant="green">
       <Hero>
-        <h1>{props.pressRelease.date}</h1>
-        <h2 className="h2 text-center">{props.pressRelease.title}</h2>
+        <h1>{date}</h1>
+        <h2 className="h2 text-center">{title}</h2>
       </Hero>
     </Block>
   );
